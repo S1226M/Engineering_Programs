d1170416ffd9febcc4c15fcfd0ea47df0384ae1e
Smit Maru <149161090+S1226M@users.noreply.github.com>
2024-04-01 23:48:42 +0530

    Add files via upload

---
+++ Sem-2/JAVA.2/Pratic/StringConcatenation.java
@@ -0,0 +1,13 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        // Define two strings
+        String str1 = "Hello";
+        String str2 = "World";
+        
+        // Concatenate the strings
+        String result = str1 + " " + str2;
+        
+        // Print the concatenated string
+        System.out.println("Concatenated string: " + result);
+    }
+}
