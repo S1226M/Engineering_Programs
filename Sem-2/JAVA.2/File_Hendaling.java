e1e62ee5c125017ab82ff0b1efb9822a7a3c1adb
Smit Maru <149161090+S1226M@users.noreply.github.com>
2024-04-15 10:46:38 +0530

    Add files via upload

---
+++ Sem-2/JAVA.2/File_Hendaling.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class File_Hendaling {
+    public static void main(String[] args) {
+
+       // Code to create a new file
+        /*
+        File myFile = new File("File_Handling_Demo_File.txt");
+        try {
+            myFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Unable to create this file");
+            e.printStackTrace();
+        }
+
+
+        // Code to write to a file
+        try {
+            FileWriter fileWriter = new FileWriter("File_Handling_Demo_File.txt");
+            fileWriter.write("This is our first file from this java course\nOkay now bye");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Reading a file
+        File myFile = new File("File_Handling_Demo_File.txt");
+        try {
+            Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        */
+        // Deleting a file
+        File myFile = new File("File_Handling_Demo_File.txt");
+        if(myFile.delete()){
+            System.out.println("I have deleted: " + myFile.getName());
+        }
+        else{
+            System.out.println("Some problem occurred while deleting the file");
+        }
+
+
+    }
+}
+
