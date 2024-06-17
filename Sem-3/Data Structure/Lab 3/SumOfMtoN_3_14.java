d05d545ed60fd355c760c0bd4cb1018004a922cf
Smit Maru <149161090+S1226M@users.noreply.github.com>
2024-06-17 19:08:15 +0530

    Add files via upload

---
+++ Sem-3/Data Structure/Lab 3/SumOfMtoN_3_14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfMtoN_3_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        System.out.println("Enter the value of m");
+        int m = sc.nextInt();
+        int ans = 0;
+        for(int i=n ; i<=m ; i++){
+            ans =ans+i;
+        }
+        System.out.println("sum of n to m :"+ans);
+    }    
+}
