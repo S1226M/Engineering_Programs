33d30c879d40b53fc42f435acf6578507bccb1d2
Smit Maru <149161090+S1226M@users.noreply.github.com>
2024-06-23 20:17:20 +0530

    Add files via upload

---
+++ Sem-3/Data Structure/Lab 1-2/FindPowerOfNumber_12_6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FindPowerOfNumber_12_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base = sc.nextInt();
+        int power = sc.nextInt();
+        int ans = 1;
+        for(int i=1 ; i<=power ; i++){
+            ans *= base; 
+        }
+        System.out.println(ans);
+    }
+}
