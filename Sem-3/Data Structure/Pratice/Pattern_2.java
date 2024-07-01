a118c44a08dd593704e4f0afe46b6fc8f6a3b7d0
Smit Maru <149161090+S1226M@users.noreply.github.com>
2024-07-01 19:03:25 +0530

    Add files via upload

---
+++ Sem-3/Data Structure/Pratice/Pattern_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern_2 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int a = sc.nextInt();
+        char c = 'a';
+        int n = 1;
+        for(int i=0 ; i<a ; i++){
+            for(int j=0 ; j<i ; j++){
+                if(i%2==0){
+                    System.out.print(c++);
+                }
+                else{
+                    System.out.print(n++);
+                }
+            }
+            System.out.println();
+        }
+    }    
+}
---
+++ Sem-3/Data Structure/Pratice/Pattern_6.java
@@ -0,0 +1,13 @@
+package Sem-3.Data Structure.Pratic;
+
+public class Pattern_6 {
+    for(int i = 2 ; i<=a ; i++){
+        for(int k=0 ; k<(a-i) ; k++){
+            System.out.print(" ");
+        }
+        for(int j=1 ; j<=(2*i)-1 ; j++){
+                System.out.print("*");
+        }
+        System.out.println();
+    }    
+}
