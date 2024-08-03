97de7261382b9637856f50f08c4792cb335964e9
Smit Maru <149161090+S1226M@users.noreply.github.com>
2024-08-03 14:10:32 -0400

    03-08-2024

---
+++ Sem-3/Data Structure/Lab 11/QueueUsingLinkList_11_61.java
@@ -0,0 +1,59 @@
+class LL {
+    class Node {
+        int info;
+        Node link;
+
+        public Node(int data) {
+            this.info = data;
+            this.link = null;
+        }
+    }
+
+    public Node first = null;
+
+    public void insertAtEnd(int x) {
+        Node newNode = new Node(x);
+        if (first == null) {
+            first = newNode;
+            return;
+        }
+        Node temp = first;
+        while (temp.link != null) {
+            temp = temp.link;
+        }
+        temp.link = newNode;
+    }
+
+    int deleteAtFirst() {
+        if (first == null) {
+            return -1;
+        } else {
+            int temp = first.info;
+            first = first.link;
+            return temp;
+        }
+    }
+
+    void display() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.print(temp.info + " ");
+            temp = temp.link;
+        }
+        System.out.println();
+    }
+}
+
+public class QueueUsingLinkList_11_61 {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+        list.insertAtEnd(50);
+        list.display();
+        list.deleteAtFirst();
+        list.display();
+    }
+}
