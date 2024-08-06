c33a837234fca510b85b81563e39bcdcd90eeb1f
S1226M <smitmaru1226@gmail.com>
2024-08-06 22:34:36 +0530

    06-08-2024

--- Sem-3/Data Structure/Lab 11/StackUsingLinkList_11_60.java
+++ Sem-3/Data Structure/Lab 11/StackUsingLinkList_11_60.java
@@ -1,18 +1,18 @@
-import org.w3c.dom.Node;
-
-class LL{
-    class Node{
+//push , pop , peek , change
+class LL {
+    class Node {
         int info;
         Node link;
 
-        public Node(int data){
-            this.info=data;
-            this.link=null;
+        public Node(int data) {
+            this.info = data;
+            this.link = null;
         }
     }
 
     public Node first = null;
 
+    // push
     public void insertAtEnd(int x) {
         Node newNode = new Node(x);
         if (first == null) {
@@ -26,6 +26,7 @@ class LL{
         temp.link = newNode;
     }
 
+    // pop
     int deleteAtLast() {
         if (first == null) {
             return -1;
@@ -44,6 +45,47 @@ class LL{
         }
     }
 
+    // peek
+    void findTheElement(int x) {
+        Node last = first;
+        int count = 1;
+        if (first == null) {
+            System.out.println("LL not found");
+            return;
+        } else if (first.link == null) {
+            System.out.println(first.info);
+            return;
+        } else {
+            while (last.link != null) {
+                last = last.link;
+                count++;
+            }
+            if (x <= 0 || x > count) {
+                System.out.println("node dose not exist");
+                return;
+            }
+            Node save = first;
+            for (int i = 1; i < (count - x + 1); i++) {
+                save = save.link;
+            }
+            System.out.println(save.info);
+        }
+    }
+
+    // change
+    void changeTheElement(int x, int y) {
+        Node temp = first;
+        while (temp.info != x) {
+            temp = temp.link;
+            if (temp.link == null && temp.info != x) {
+                System.out.println("node does not exist");
+                return;
+            }
+        }
+        temp.info = y;
+        System.out.println(temp.info);
+    }
+
     void display() {
         Node temp = first;
         while (temp != null) {
@@ -53,6 +95,7 @@ class LL{
         System.out.println();
     }
 }
+
 public class StackUsingLinkList_11_60 {
     public static void main(String[] args) {
         LL list = new LL();
@@ -60,7 +103,11 @@ public class StackUsingLinkList_11_60 {
         list.insertAtEnd(30);
         list.insertAtEnd(20);
         list.insertAtEnd(10);
-        list.deleteAtLast();
+        list.insertAtEnd(5);
+        // list.deleteAtLast();
+        // list.display();
+        // list.findTheElement(5);
+        list.changeTheElement(15, 0);
         list.display();
-    }    
+    }
 }
