82411010eef85f70bdd0a76bf3d716e39a0e86f4
S1226M <smitmaru1226@gmail.com>
2024-08-08 23:29:04 +0530

    08-08-2024

---
+++ Sem-3/Data Structure/Lab 13/DeleteAlternet_13_73.java
@@ -0,0 +1,87 @@
+class ll {
+    class Node {
+        int info;
+        Node lptr;
+        Node rptr;
+
+        Node(int data) {
+            this.info = data;
+            this.lptr = null;
+            this.rptr = null;
+        }
+    }
+
+    private Node first = null;
+
+    void DLLIF(int x) {
+        Node newNode = new Node(x);
+        if (first == null) {
+            first = newNode;
+        } else {
+            newNode.rptr = first;
+            first.lptr = newNode;
+            first = newNode;
+        }
+    }
+
+    void deleteAtAlternet(){
+        Node nodeCount = first;
+        int count = 1;
+        while(nodeCount.rptr != null){
+            nodeCount = nodeCount.rptr;
+            count++;
+        }
+        if(count >= 3){
+            Node temp = first;
+            Node save = temp.rptr.rptr;
+        } else if(count ==2){
+            System.out.println(first.info);
+            return;
+        } else {
+            System.out.println("In LL only one node");
+            return;
+        }
+        if(count % 2 == 0){
+            Node temp = first;
+            Node save = temp.rptr.rptr;
+            while(save.rptr.rptr != null){
+                temp.rptr = save;
+                save.lptr = temp;
+                temp = save;
+                save = save.rptr.rptr;
+            }
+            save.rptr = null;
+        } else {
+            Node temp = first;
+            Node save = temp.rptr.rptr;
+            for(int i=1 ; i <= (count/2) ; i++){
+                temp.rptr = save;
+                save.lptr = temp;
+                temp = save;
+                save = save.rptr.rptr;
+            }
+        }
+
+        void display() {
+            Node temp = first;
+            while (temp != null) {
+                System.out.print(temp.info + " ");
+                temp = temp.rptr;
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class DeleteAlternet_13_73 {
+    public static void main(String[] args) {
+        ll list = new ll();
+        list.DLLIF(50);
+        list.DLLIF(40);
+        list.DLLIF(30);
+        list.DLLIF(20);
+        list.DLLIF(10);
+        list.deleteAtAlternet();
+        list.display();
+    }
+}
---
+++ Sem-3/Web Technology/ExpressJS/Lab 19/HelloWord_19_1.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+}).listen(1234,()=>{
+    console.log("server start at 1234")
+});
