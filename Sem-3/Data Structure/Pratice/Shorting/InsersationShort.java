3bcb12ea7af8fe5be43b245df75f060d90c6f284
S1226M <smitmaru1226@gmail.com>
2024-09-10 23:14:03 +0530

    10-09-2024

---
+++ Sem-3/Data Structure/Pratice/Shorting/InsersationShort.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class InsersationShort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0 ; i < n ; i++){
+            System.out.print("Enter the element : "+(i+1));
+            a[i] = sc.nextInt();
+        }
+        //5,4,3,2,1,0
+        for(int i = 0 ; i < n ; i++){
+            while()
+        }      
+    }
+}
