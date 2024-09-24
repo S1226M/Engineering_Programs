5ee4e3bd00a1b6151ae120deeb391d9fcb18c82e
S1226M <smitmaru1226@gmail.com>
2024-09-24 22:44:54 +0530

    24-09-2024

---
+++ Sem-3/Python/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+s = "smit" # here we can itrate the string
+# for i in s:
+#     print(i)
+
+# t = [1,4,6,234,6,764]
+# for i in t:
+#     print(i)
+
+# l = [1,2,3]
+# for item in l:
+#     print(item)
+
+# else:
+#     print("Done")
