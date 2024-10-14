af40fcbec953c872f1e29d7f2c882be89514031a
S1226M <smitmaru1226@gmail.com>
2024-10-14 22:22:29 +0530

    14-10-2024

--- Sem-3/Data Structure/SearchAndShort/BubbleShort.java
+++ Sem-3/Data Structure/SearchAndShort/BubbleShort.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SelectionShort {
+public class BubbleShort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the sie of the array");
@@ -12,19 +12,19 @@ public class SelectionShort {
         }
 
         //[5,2,6,4,1]
-        for(int  i = 0 ; i<n-1 ; i++){
-           for(int j=i+1 ; j<n ; j++){
-            if(arr[i]>arr[j]){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for(int i=0 ; i<n-1 ; i++){
+            for(int j=0 ; j<n-i-1 ; j++){
+                if(arr[j]  > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
             }
-           }
         }
 
         System.out.print("Sorted array: ");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-    }
+    }    
 }
