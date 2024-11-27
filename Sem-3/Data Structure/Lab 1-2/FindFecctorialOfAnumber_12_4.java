5245302a0db768ad1170b57fa4d9d3873fdbb8fd
S1226M <smitmaru1226@gmail.com>
2024-11-27 19:20:17 +0530

    27-11-2024

--- Sem-3/Data Structure/Lab 1-2/FindFecctorialOfAnumber_12_4.java
+++ Sem-3/Data Structure/Lab 1-2/FindFecctorialOfAnumber_12_4.java
@@ -12,3 +12,30 @@ public class FindFecctorialOfAnumber_12_4 {
         }
     }
 }
+
+
+
+// import java.util.Scanner;
+
+// class factorial{
+//     int fac(int a){
+//         if(a==0 || a==1){
+//             return 1;
+//         }
+//         return a * fac(a-1);
+//     }
+// }
+// public  class FindFecctorialOfAnumber_12_4{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         factorial f1 = new factorial();
+//         int number = sc.nextInt();
+//         if( number < 0 ){
+//             System.out.println("Factorial is not possible for negative number");
+//         }
+//         else{
+//             int ans = f1.fac(number);
+//             System.out.println();
+//         }
+//     }
+// }
--- Sem-3/Data Structure/Lab 1-2/TakeDayAndPrintYearMonthWeekDay_12_10.java
+++ Sem-3/Data Structure/Lab 1-2/TakeDayAndPrintYearMonthWeekDay_12_10.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class TakeDayAndPrintYearMonthWeekDay_12_10 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            float inputDay = sc.nextInt();
-            double year1 = (inputDay/365);
-            int year = (int)year1;
-            double year2 = year1 - year;
+            float inputDay = sc.nextInt();//397
+            double year1 = (inputDay/365);//1.087.....
+            int year = (int)year1;//1
+            double year2 = year1 - year;//0.87...
 
-            double month1 = year2 * 12;
-            int month = (int)month1;
-            double month2 = month1 - month;
+            double month1 = year2 * 12;//1.052054....
+            int month = (int)month1;//1
+            double month2 = month1 - month;//0.052054....
 
-            double week1 = month2 * 4;
-            int week = (int)week1;
-            double week2 = week1 - week;
+            double week1 = month2 * 4;//0.20821...
+            int week = (int)week1;//0
+            double week2 = week1 - week;//0.2021...
 
-            double day1 = week2 * 7;
-            int day = (int)day1;
+            double day1 = week2 * 7;//1.457...
+            int day = (int)day1;//1
             System.out.println("Year:"+year+", Month:"+month+", Week:"+week+", Day"+day);
         }
     }
