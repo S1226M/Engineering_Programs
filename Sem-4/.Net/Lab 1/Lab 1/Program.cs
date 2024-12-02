3b7c858ab92d1f2a220bc35e75c2ee29b728e203
S1226M <smitmaru1226@gmail.com>
2024-12-02 18:57:22 +0530

    02-12-2024

---
+++ Sem-4/.Net/Lab 1/Lab 1/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1.Write a program to print your name, address, contact number & city.
+            //Console.WriteLine("Smit");
+            //Console.WriteLine("Rajkot");
+            //Console.WriteLine("8780031119");
+            //Console.WriteLine("Rajkot");
+
+
+            ////2.Write a program to get two numbers from user and print those two numbers.
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Number a : ");
+            //Console.WriteLine(a);
+            //Console.Write("Number b : ");
+            //Console.WriteLine(b);
+
+            ////3.Write program to prompt a user to input his/ her name and country name and then output will be shown as given: Hello<yourname> from <countryname>
+            //String name = Console.ReadLine();
+            //String country = Console.ReadLine();
+            //Console.WriteLine("Hello "+name+" from "+country);
+
+            //4.Write a program to calculate the size of the area in square - feet based Specified length and width.
+            //Console.WriteLine("Enter length");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter height");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(a*b);
+
+            ////5.Write a program to calculate area of Square, Rectangle and Circle.
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //switch (a) {
+            //    case 1:
+            //        Console.Write("Enter length : ");
+            //        int length = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine("Area of the square : " + (length * length));
+            //        break;
+            //    case 2:
+            //        Console.Write("Enter length : ");
+            //        int lengthR = Convert.ToInt32(Console.ReadLine());
+            //        Console.Write("Enter Width : ");
+            //        int width = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine("Area of the Reactangle : "+(lengthR*width));
+            //        break;
+            //    case 3:
+            //        Console.Write("Enter radious : ");
+            //        int radious = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine("Area of the Reactangle : " + (3.14*radious*radious));
+            //        break;
+            //}
+
+            ////6.Write a program to calculate Celsius to Fahrenheit and vice - versa using function.
+            ////f = c*(9/5) + 32
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //switch (a) {
+            //    case 1:
+            //        Console.Write("Enter the value of fahrenheit : ");
+            //        int f = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine((f-32)*(5/9));
+            //        break;
+            //    case 2:
+            //        Console.Write("Enter the value of Celsius : ");
+            //        int c = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine(c*(9/5)+32);
+            //        break;
+            //}
+
+            ////7.Write a program to find out Simple Interest using function. (I = PRN / 100) 
+            //double P = Convert.ToDouble(Console.ReadLine());
+            //double R = Convert.ToDouble(Console.ReadLine());
+            //double N = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine((P * R * N) / 100);
+
+            //8. Write a program to create a Simple Calculator for two numbers (Addition Multiplication, Subtraction, Division) [Also using if…else or Switch Case]
+            //Console.WriteLine("Enter 1 for Addition");
+            //Console.WriteLine("Enter 2 for Multiplication");
+            //Console.WriteLine("Enter 3 for Subtraction");
+            //Console.WriteLine("Enter 4 for Division");
+            //Console.WriteLine();
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter numebr One");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter numebr Two");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //if (x == 1)
+            //{
+            //    Console.Write("Addtion : "+(a+b));
+            //}
+            //else if (x == 2)
+            //{
+            //    Console.Write("Multiplication : "+(a * b));
+            //}
+            //else if (x == 1)
+            //{
+            //    Console.Write("Subtraction : "+(a - b));
+            //}
+            //else if (x == 1)
+            //{
+            //    Console.Write("Division : "+(a / b));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid number");
+            //}
+
+            //9.Write a program to Swapping without using third variable. 
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a : " + (b));
+            Console.WriteLine("b : " + ())
+        }   
+    }
+}
