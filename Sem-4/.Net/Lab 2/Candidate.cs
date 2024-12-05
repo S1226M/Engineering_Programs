c79451bff6d766b9dbebafa6604db7f060ce09ab
S1226M <smitmaru1226@gmail.com>
2024-12-05 20:11:25 +0530

    05-12-2024

---
+++ Sem-4/.Net/Lab 2/Candidate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class Candidate
+    {
+        int id;
+        string name;
+        int age;
+        double weight;
+        double height;
+
+        public void GetCandidateDetails()
+        {
+            id = Convert.ToInt32(Console.ReadLine());
+            name = Console.ReadLine();
+            age = Convert.ToInt32(Console.ReadLine());
+            weight = Convert.ToDouble(Console.ReadLine());
+            height = Convert.ToDouble(Console.ReadLine());
+        }
+         public void DisplayCandidateDetails()
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(name);
+            Console.WriteLine(age); 
+            Console.WriteLine(weight);
+            Console.WriteLine(height);
+        }
+    }
+}
---
+++ Sem-4/.Net/Lab 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question.1
+            //Candidate c = new Candidate();
+            //c.GetCandidateDetails();
+            //c.DisplayCandidateDetails();
+
+            //Question.2
+            //Staff[] s = new Staff[5];
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    s[i] = new Staff();
+            //    s[i].GetStaffDetails();
+            //}
+            //Console.WriteLine("print");
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    if (s[i].Designation == "hod")
+            //    {
+            //        s[i].DisplayStaffDetails();
+            //    }
+            //}
+
+            //Question.3
+            //Bank_Account b = new Bank_Account();
+            //b.GetDetails();
+            //b.printDetails();
+
+            //Question.4
+            //Student s = new Student(23,"smit",5,6,10);
+            //s.print();
+
+
+        }
+    }
+}
