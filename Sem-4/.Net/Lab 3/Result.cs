d5fca480b7832d8bbd3a9c4a60a472479668ea48
S1226M <smitmaru1226@gmail.com>
2024-12-09 18:26:29 +0530

    09-12-2024

---
+++ Sem-4/.Net/Lab 3/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    interface Result
+    {
+        int Add(int a , int b);
+        int Sub(int a , int b);
+    }
+    class Ans : Result
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Sem-4/.Net/Lab 3/Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    abstract class Sum
+    {
+        public abstract void SumOfTwo(int a, int b);
+        public abstract void SumOfThree(int a, int b, int c);
+    }
+    class Calculate : Sum
+    {
+        public override void SumOfTwo(int a, int b)
+        {
+            int ans = a + b;
+            Console.WriteLine(ans);
+        }
+        public override void SumOfThree(int a, int b, int c)
+        {
+            int ans = a + b + c;
+            Console.WriteLine(ans);
+        }
+    }
+}
