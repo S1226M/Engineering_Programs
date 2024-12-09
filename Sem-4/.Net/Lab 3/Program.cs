5761f4365da55fa397eb85a30059cd12703898fd
S1226M <smitmaru1226@gmail.com>
2024-12-09 18:26:29 +0530

    09-12-2024

---
+++ Sem-4/.Net/Lab 3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Que 1
+            //int a = 1;
+            //int b = 0;
+            //try
+            //{
+            //    Console.WriteLine(a / b);
+            //}
+            //catch (DivideByZeroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            //finally 
+            //{ 
+            //    Console.WriteLine(a / b);
+            //}
+
+            //Que 2
+            //int[] a = new int[2];
+            //try
+            //{
+            //    for (int i = 0; i <= a.Length; i++)
+            //    {
+            //        a[i] = i;
+            //    }
+            //}
+            //catch (IndexOutOfRangeException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //Que 3
+            //Sum s = new Calculate();
+            //s.SumOfTwo(20, 30);
+            //s.SumOfThree(20, 30 , 50);
+
+            //Que 4
+            //Result r = new Ans();
+            //int a = r.Add(10, 20);
+            //Console.WriteLine(a);
+            //int b = r.Sub(10, 20);
+            //Console.WriteLine(b);
+
+            //Que 5
+            //String s = "Darshan University";
+            //String st = "Rajkot";
+            //Console.WriteLine(s.Length);
+            //Console.WriteLine(s.ToUpper());
+            //Console.WriteLine(s.ToLower());
+            //Console.WriteLine(string.Concat(s,st));
+            //Console.WriteLine(s[0]);
+            //Console.WriteLine(s.IndexOf("a"));
+            //Console.WriteLine(s.Substring(9));
+
+            //Que 6
+            String s = "AbCdEfGh";
+            for (int i = 0; i < s.Length; i++) 
+            {
+                if (s[i].IsUpper())
+                {
+
+                }
+            }
+        }
+    }
+}
