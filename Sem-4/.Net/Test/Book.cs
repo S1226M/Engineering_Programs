6eee997ebac12329b9cd9e161434f84a8d7e14bd
S1226M <smitmaru1226@gmail.com>
2024-12-23 19:28:48 +0530

    23-12-2024

---
+++ Sem-4/.Net/Test/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Book
+    {
+        int BookID;
+        string Title;
+        string Author;
+        public Book(int id,string t,string a)
+        {
+            BookID = id;
+            Title = t;
+            Author = a;
+        }
+
+        public void getDetails()
+        {
+            Console.Write("Enter Book ID :");
+            BookID = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Book Title :");
+            Title = Console.ReadLine();
+            Console.Write("Enter Book Author Name :");
+            Author = Console.ReadLine();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Book ID : "+BookID);
+            Console.WriteLine("Book Title : "+Title);
+            Console.WriteLine("Book Author : " +Author);
+        }
+    }
+}
---
+++ Sem-4/.Net/Test/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Employee
+    {
+        int EmployeeID;
+        string Name;
+        double Salaries;
+
+        public void InputDetails()
+        {
+            Console.Write("Enter Employee ID :");
+            EmployeeID = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Name :");
+            Name = Console.ReadLine();
+            Console.Write("Enter Monthly Salaries :");
+            Salaries = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void DisplayDEtails()
+        {
+            Console.WriteLine("Employee id = " + EmployeeID);
+            Console.WriteLine("Employee Name = " + Name);
+            Console.WriteLine("Employee Monthly salary = " + Salaries);
+        }
+
+        public double CalculateAnualSalary(double s)
+        {
+            return s*12;
+        }
+
+        public void DeterminePermance(double s)
+        {
+            if(s > 1000000)
+            {
+                Console.WriteLine("Outstanding");
+            }
+            else if(s >= 500000 &&  s <= 1000000)
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if(s >= 250000 && s<= 499999)
+            {
+                Console.WriteLine("Good");
+            }
+            else
+            {
+                Console.WriteLine("Need to improve");
+            }
+        }
+    }
+}
