0e5eec089ea1b6a4a2d951e281bcc30509139832
S1226M <smitmaru1226@gmail.com>
2024-12-24 18:20:47 +0530

    24-12-2024

---
+++ Sem-4/.Net/MVC Lab-6.1/Controllers/FormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Lab_6._1.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
