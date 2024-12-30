699091fd6ef45ec9a63f02c0ddc3fb8cc3cda3ac
S1226M <smitmaru1226@gmail.com>
2024-12-30 19:52:55 +0530

    30-12-2024

---
+++ Sem-4/.Net/Pratice 1/Time.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratice_1
+{
+    public class Time
+    {
+        int s;
+        int m;
+        int h;
+        public Time() 
+        {
+            s = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            h = Convert.ToInt32(Console.ReadLine());
+        }
+        public Time(Time other)
+        {
+            Console.WriteLine(other.h+":"+other.m+":"+other.s);
+        }
+
+        public void Add(int s, int m, int h) 
+        {
+            this.s = s + s;
+            if (s > 60)
+            {
+                m++;
+                s = s - 60;
+            }
+            this.m = m + m;
+            if (m > 60)
+            {
+                h++;
+                m = m - 60;
+            }
+            this.h = h + h;
+            Console.WriteLine(h+":"+m+":"+s);
+        }
+    }
+}
