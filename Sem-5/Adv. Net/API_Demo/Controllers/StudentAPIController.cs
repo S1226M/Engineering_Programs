be973e9ce4f0b474a9067a265b026801012e406c
S1226M <smitmaru1226@gmail.com>
2025-06-05 20:56:06 +0530

    05-06-2025

---
+++ Sem-5/Adv. Net/API_Demo/Controllers/StudentAPIController.cs
@@ -0,0 +1,33 @@
+﻿//using ASPCoreWebAPICRUD.Models; 
+using API_Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly Cse5AdvNetContext context;
+
+        public StudentAPIController(Cse5AdvNetContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudent()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            //var student = await cont
+        }
+    }
+}
---
+++ Sem-5/Adv. Net/SQLQuery1.sql
@@ -0,0 +1,23 @@
+Create Table Student(
+	ID Int,
+	StudentName varchar(50),
+	StudentGender varchar(20),
+	Age	int,
+	Standard varchar(10),
+	FatherName varchar(50)
+)
+
+Insert Into Student(ID, StudentName, StudentGender, Age, Standard, FatherName)
+VALUES
+	(1, 'Aarav Mehta', 'Male', 14, '8th', 'Rakesh Mehta'),
+	(2, 'Ananya Sharma', 'Female', 13, '7th', 'Sanjay Sharma'),
+	(3, 'Kabir Singh', 'Male', 15, '9th', 'Rajeev Singh'),
+	(4, 'Isha Patel', 'Female', 12, '6th', 'Kiran Patel'),
+	(5, 'Vivaan Roy', 'Male', 14, '8th', 'Amit Roy'),
+	(6, 'Diya Verma', 'Female', 13, '7th', 'Manoj Verma'),
+	(7, 'Rehan Khan', 'Male', 15, '9th', 'Salman Khan'),
+	(8, 'Meera Nair', 'Female', 12, '6th', 'Suresh Nair'),
+	(9, 'Yash Rathore', 'Male', 14, '8th', 'Mahesh Rathore'),
+	(10, 'Simran Kaur', 'Female', 13, '7th', 'Harpreet Kaur');
+
+Select * From Student 
