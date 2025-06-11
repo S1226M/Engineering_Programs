9142019b9aa4464637d8c369611f76b5ea6a9874
S1226M <smitmaru1226@gmail.com>
2025-06-11 22:31:44 +0530

    11-06-2025

--- Sem-5/DAA/Queue.c
+++ Sem-5/DAA/Queue.c
@@ -8,6 +8,7 @@ void Enqueue(int data)
     if (rear == size - 1)
     {
         printf("Queue is full\n");
+        return;
     }
     else
     {
@@ -23,20 +24,73 @@ void Enqueue(int data)
     }
 }
 
+int Dequeue()
+{
+    int Dequeuedata;
+    if (front == -1)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        Dequeuedata = queue[front];
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        else
+        {
+            front = front + 1;
+        }
+    }
+    return Dequeuedata;
+}
+
 void display()
 {
+    int i;
+    if (front == -1)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        printf("Queue elements are:\n");
+        for (int i = front; i <= rear; i++)
+        {
+            printf("%d ", queue[i]);
+        }
+    }
 }
 void main()
 {
+    int n;
+    int data;
+    int Ans;
     while (1)
     {
-        printf("Enter 1 for enqueue")
-            printf("Enter 2 for dequeue")
-                printf("Enter 3 for display")
-                    printf("Enter n :") switch (n)
+        printf("Enter 1 for enqueue");
+        printf("Enter 2 for dequeue");
+        printf("Enter 3 for display");
+        printf("Enter n :");
+        scanf("%d", &n);
+        switch (n)
         {
         case 1:
+            printf("Enter data to be inserted: ");
+            scanf("%d", &data);
+            Enqueue(data);
+            break;
 
+        case 2:
+            printf("Dequeue Element is :");
+            Ans = Dequeue();
+            printf("%d\n", &Ans);
+            break;
+
+        case 3:
+            display();
             break;
 
         default:
