22b273a8d03f6f9b0dfe0c3fa425b5c3e76bac08
S1226M <smitmaru1226@gmail.com>
2025-06-18 13:36:42 +0530

    18-06-2025

---
+++ Sem-5/DAA/InsertionSort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void InsertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+void main(){
+    int arr[] = {10,8,1,50,80,40};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    InsertionSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
