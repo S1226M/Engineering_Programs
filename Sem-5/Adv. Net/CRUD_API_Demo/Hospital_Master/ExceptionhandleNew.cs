b7adbda15feef8420e4c6f61ef06291ffc018bc1
S1226M <smitmaru1226@gmail.com>
2025-06-20 21:04:49 +0530

    20-06-2025

---
+++ Sem-5/Adv. Net/CRUD_API_Demo/Hospital_Master/ExceptionhandleNew.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace CRUDAPIDemo
+{
+    public class ExceptionhandleNew
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionhandleNew(RequestDelegate next)
+
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, Exception ex)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch
+            {
+                await HandleExceptioinAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptioinAsync(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; // 500
+            context.Response.ContentType = "application/json";
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(new
+            {
+                //StatusCode = context.Response.StatusCode,
+                Message = "Exception Come",
+                Status = "Error" // You might want to log this instead of sending it to the client
+            }));
+        }
+    }
+}
---
+++ Sem-5/Adv. Net/CRUD_API_Demo/Hospital_Master/Middleware.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CRUDAPIDemo
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Middleware
+    {
+        private readonly RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+            return context.Response.WriteAsJsonAsync(JsonConvert.SerializeObject(
+                new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = ex.Message,
+                    Status = "ERROR"
+                }));
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+
+}
