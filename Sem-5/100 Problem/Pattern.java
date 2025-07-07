f1e770754b811fe544462ccf87a4ef3bffd1a09d
Smit Maru <smitmaru1226@gmail.com>
2025-07-07 21:01:17 +0530

    Pattern,Fibonacci,OddEven Problem Solved Out Of 100

--- Sem-5/100 Problem/Pattern.java
+++ Sem-5/100 Problem/Pattern.java
@@ -1,19 +1,12 @@
-
 public class Pattern {
-
     public static void main(String[] args) {
-        int numberOfRows = 5; // You can change this value to print more or fewer rows
+        int numberOfRows = 5;
         for (int i = 1; i <= numberOfRows; i++) {
-            // Print leading spaces
-            for (int j = 1; j <= numberOfRows - i; j++) {
+            for (int j = 1; j <= (numberOfRows-(numberOfRows - i)); j++){
+                System.out.print(j+" "); 
                 System.out.print(" ");
-            }
-            // Print stars
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            // Move to the next line after each row
-            System.out.println();
+            } 
+            System.out.println(" ");
         }
     }
 }
