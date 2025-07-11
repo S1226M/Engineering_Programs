cc8911587b703be506e162f142ab264236ca39e6
Smit Maru <smitmaru1226@gmail.com>
2025-07-11 22:38:56 +0530

    Code Mearge

--- Sem-5/100 Problem/Factor_Of_Number.java
+++ Sem-5/100 Problem/Factor_Of_Number.java
@@ -1,16 +1,42 @@
+
 import java.util.Scanner;
 
-public class Factor_Of_Number{
+public class Factor_Of_Number {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         int Fac = 1;
         System.out.print("Factors of " + n + " are: ");
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 System.out.print(i + " ");
             }
         }
     }
 }
+
+// import java.util.Scanner;
+// public class Factor_Of_Number_Rec {
+//     static void printFactors(int n, int i) {
+//         if (i > n) {
+//             return;
+//         }
+//         if (n % i == 0) {
+//             System.out.print(i + " ");
+//         }
+//         printFactors(n, i + 1);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+//         if (n <= 0) {
+//             System.out.println("Please enter a positive integer.");
+//         } else {
+//             System.out.print("Factors of " + n + " are: ");
+//             printFactors(n, 1);
+//         }
+//     }
+// }
