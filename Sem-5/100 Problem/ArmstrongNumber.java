26ea0e7062384af8807c330211c3e9fe90ce559c
Smit Maru <smitmaru1226@gmail.com>
2025-07-21 22:28:43 +0530

    Armstrong number code is added in the 100 problem

--- Sem-5/100 Problem/ArmstrongNumber.java
+++ Sem-5/100 Problem/ArmstrongNumber.java
@@ -1,16 +1,26 @@
-
 import java.util.*;
 
 public class ArmstrongNumber {
 
-    Scanner sc = new Scanner(System.in);
-    String n = sc.nextLine();
-    int len = n.length();
-    int sum = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String n = sc.nextLine();
+        int len = n.length();
+        int sum = 0;
 
+        for (int i = 0; i < len; i++) {
+            int digit = n.charAt(i) - '0'; // convert char to int
+            sum += Math.pow(digit, len);   // raise to power of number of digits
+        }
 
-for(int i=0 ; i<len ; i++){
-        int digit = n.charAt(i) - '0';
-        sum += pow(digit,len);
+        int original = Integer.parseInt(n);
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+
+        sc.close();
     }
 }
