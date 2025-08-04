477e5de005936fa4eceef2aa562d4fbfd3b1de9f
Smit Maru <smitmaru1226@gmail.com>
2025-08-04 23:44:06 +0530

    100 Problem code id added and some changes is added

---
+++ Sem-5/100 Problem/MaxMinAverageWithoutArray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MaxMinAverageWithoutArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers (n): ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Please enter a positive number.");
+            return;
+        }
+
+        // Initialize variables
+        System.out.print("Enter number 1: ");
+        int num = sc.nextInt();
+        int max = num;
+        int min = num;
+        int sum = num;
+
+        // Loop starts from 2 since 1st value already taken
+        for (int i = 2; i <= n; i++) {
+            System.out.print("Enter number " + i + ": ");
+            num = sc.nextInt();
+
+            if (num > max)
+                max = num;
+            if (num < min)
+                min = num;
+
+            sum += num;
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+        System.out.println("Average: " + average);
+
+        sc.close();
+    }
+}
---
+++ Sem-5/DAA/Prim's Algo/RevLinkList.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+void display(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->info);
+        ptr = ptr->link;
+    }
+    printf("NULL\n");
+}
+
+void reverse(struct node *temp)
+{
+    struct node *first = temp;
+    struct node *second = temp->link;
+    struct node *last = temp;
+    int count = 1;
+    while (last->link != NULL)
+    {
+        last = last->link;
+        count++;
+    }
+    printf("%d", last->info);
+
+    for (int i = 0; i < count; i++)
+    {
+        last->link = first;
+        last = first;
+        first = second;
+        last->link = NULL;
+        second = second->link;
+    }
+
+    printf("%d", first->info);
+    printf("%d", second->info);
+    printf("%d", last->info);
+}
+
+void main()
+{
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *first = (struct node *)malloc(sizeof(struct node));
+    struct node *secound = (struct node *)malloc(sizeof(struct node));
+    struct node *third = (struct node *)malloc(sizeof(struct node));
+    struct node *forth = (struct node *)malloc(sizeof(struct node));
+    head->info = 32;
+
+    head->link = first;
+    first->info = 33;
+    first->link = secound;
+
+    secound->info = 22;
+    secound->link = third;
+
+    third->info = 25;
+    third->link = forth;
+
+    forth->info = 18;
+    forth->link = NULL;
+
+    display(head);
+    reverse(head);
+}
