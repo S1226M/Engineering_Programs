25afea3393e115e6629cfa5b2961c126d4ce3e2d
Smit Maru <smitmaru1226@gmail.com>
2025-09-02 22:23:07 +0530

    PDF is Added of the CN and Other

--- Sem-3/Data Structure/Pratice/Pattern_2.java
+++ Sem-3/Data Structure/Pratice/Pattern_2.java
@@ -4,7 +4,8 @@ public class Pattern_2 {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
         int a = sc.nextInt();
-        char c = 'a';
+        char c = 'A';
+        System.out.print(c);
         int n = 1;
         for(int i=0 ; i<a ; i++){
             for(int j=0 ; j<i ; j++){
