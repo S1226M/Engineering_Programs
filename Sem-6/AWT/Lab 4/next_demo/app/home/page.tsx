9615f974119ea2cbb81013541cf5a19d0b19fe26
Smit Maru <smitmaru1226@gmail.com>
2025-12-05 22:35:25 +0530

    MLDL Task done AWT Project Pratice.

---
+++ Sem-6/AWT/Lab 4/next_demo/app/home/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+function HomePage() {
+  return (
+    <>
+        <h1>Home Page</h1>
+        <br />
+        <Link href="/">Go to Dashboard</Link> 
+    </>
+  )
+}
+
+export default HomePage
---
+++ Sem-6/AWT/Lab 4/next_demo/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <div className="d-flex gap-3 align-items-center py-3">
+        <Link href="/home" className="text-decoration-none text-primary fw-semibold">
+          Home
+        </Link>
+
+        <span className="text-secondary">|</span>
+
+        <Link href="/about" className="text-decoration-none text-primary fw-semibold">
+          About
+        </Link>
+      </div>
+    </>
+
+  );
+}
